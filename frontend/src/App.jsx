a4f67d0bf1fa2f9ba178c4d7dd654cfad88cb518
Yuliang Liu <yliu292@u.rochester.edu>
2025-11-15 01:17:21 -0500

    Revert "feat: api 1 using for front end"
    This reverts commit ef6087a97c6715c0e7724bc8d1ba7d8bd792a537.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import './App.css'; // (如果你们有 App.css 的话)
-import CourseGraph from './components/CourseGraph'; // <-- 导入图表组件
+import CourseGraph from "./components/CourseGraph";
 
 function App() {
-  
+  const handleNodeSelect = (id, node) => {
+    console.log("Selected course:", id, node);
+  };
+
   return (
-    <div className="App">
-      
-      {/* 我们可以加一个标题 */}
-      <header style={{ padding: '20px', textAlign: 'center' }}>
-        <h1>DandyHacks 课程规划器</h1>
-      </header>
-      
-      {/* --- 在这里渲染图表 --- */}
-      {/* 我们直接调用 CourseGraph，不传任何多余的 props */}
-      <CourseGraph />
-      
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <CourseGraph onNodeSelect={handleNodeSelect} />
     </div>
   );
 }
